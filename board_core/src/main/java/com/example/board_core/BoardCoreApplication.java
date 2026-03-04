13704ea9d20ea915d4a6d65ad5bf61e4212d0e0c
kjh <rlawngud1999@gmail.com>
2026-03-04 12:33:31 +0900

    [Feat] board_core 모듈 추가

---
+++ board_core/src/main/java/com/example/board_core/BoardCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.board_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardCoreApplication.class, args);
+    }
+
+}
